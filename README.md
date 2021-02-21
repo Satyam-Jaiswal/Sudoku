45116c61cada5d3a79c1c483af8007f870ecdd87
Satyam Jaiswal <satyam.jaiswal26@gmail.com>
2021-02-21 11:12:46 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Sudoku
+
+A Sudoku game application powered by Flutter.
+
+<div class="row">
+      <img src="/screenshot/Screenshot_1613883469.png" width="250" >
+      <img src="/screenshot/Screenshot_1613883647.png" width="250">     
+      <img src="/screenshot/Screenshot_1613883656.png" width="250" >
+</div>
---
+++ lib/GameScreen.dart
@@ -0,0 +1,221 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+import 'Board.dart';
+
+class GameMenu extends StatefulWidget {
+  final double fieldSize = 40;
+  final Color borderColor = Colors.grey;
+
+  @override
+  State<StatefulWidget> createState() => _GameMenuState();
+}
+
+class _GameMenuState extends State<GameMenu> {
+  Board board = Board.empty();
+  Field focussed;
+  bool win = false;
+  bool finished = false;
+
+  @override
+  initState() {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    super.initState();
+  }
+
+  _GameMenuState() {
+    board.load().then((loaded) {
+      setState(() {
+        if (!loaded) createNewBoard();
+      });
+    });
+  }
+
+  void createNewBoard() {
+    board = Board.modify(Random().nextInt(1 << 32));
+    focussed = null;
+    win = false;
+    finished = false;
+  }
+
+  void checkBoard() {
+    if (!board.hasEmpty() || finished) {
+      finished = true;
+      if (board.checkBoard()) {
+        setState(() {
+          win = true;
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      child: Scaffold(
+        body: Stack(children: <Widget>[
+          GestureDetector(
+            behavior: HitTestBehavior.opaque,
+            onTap: () {
+              setState(() {
+                focussed = null;
+              });
+            },
+          ),
+
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Text(!win ? "Sudoku" : "Congratulations, you won!",
+                  style: TextStyle(
+                      fontSize: 20, color: win ? Colors.green : null)),
+            )
+          ]),
+          // Reload button
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 32, horizontal: 8),
+                  child: IconButton(
+                    icon: Icon(Icons.autorenew),
+                    onPressed: () => setState(() => createNewBoard()),
+                  ))
+            ],
+          ),
+          // Game board
+          Center(
+              child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: board.fields
+                      .map(
+                        (list) => Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: list
+                                .map(
+                                  (field) => Container(
+                                      child: InkWell(
+                                        onTap: () {
+                                          setState(() {
+                                            if (focussed == field ||
+                                                field.initial)
+                                              focussed = null;
+                                            else
+                                              focussed = field;
+                                          });
+                                        },
+                                        child: Center(
+                                            child: Text(
+                                          field.number == null
+                                              ? ""
+                                              : field.number.toString(),
+                                          style: TextStyle(
+                                              fontSize: 30,
+                                              color: field.initial
+                                                  ? Theme.of(context)
+                                                      .primaryColor
+                                                  : null),
+                                        )),
+                                      ),
+                                      width: widget.fieldSize,
+                                      height: widget.fieldSize,
+                                      decoration: BoxDecoration(
+                                        color: () {
+                                          if (focussed == field) {
+                                            return Theme.of(context)
+                                                .primaryColor;
+                                          } else {
+                                            if (!field.valid) {
+                                              if (field.initial) {
+                                                return Colors.yellow;
+                                              } else {
+                                                return Colors.red;
+                                              }
+                                            } else
+                                              return Colors.white.withAlpha(50);
+                                          }
+                                        }(),
+                                        border: Border(
+                                            left: BorderSide(
+                                                color: widget.borderColor,
+                                                width:
+                                                    field.x % 3 == 0 ? 2 : 0),
+                                            right: BorderSide(
+                                                color: widget.borderColor,
+                                                width: field.x ==
+                                                        Board.boardBase - 1
+                                                    ? 2
+                                                    : 0),
+                                            top: BorderSide(
+                                                color: widget.borderColor,
+                                                width:
+                                                    field.y % 3 == 0 ? 2 : 0),
+                                            bottom: BorderSide(
+                                                color: widget.borderColor,
+                                                width: field.y ==
+                                                        Board.boardBase - 1
+                                                    ? 2
+                                                    : 0)),
+                                      )),
+                                )
+                                .toList()),
+                      )
+                      .toList())),
+          // Number pad
+          (focussed != null
+              ? Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                      Center(
+                          child: Padding(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                        child: GridView.builder(
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 5,
+                                  childAspectRatio: 1.6,
+                                  mainAxisSpacing: 1),
+                          itemCount: Board.boardBase + 1,
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          padding: const EdgeInsets.all(1),
+                          itemBuilder: (context, i) {
+                            return Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 2.0),
+                              child: OutlineButton(
+                                child: (i == Board.boardBase
+                                    ? Icon(Icons.delete)
+                                    : Text(
+                                        (i + 1).toString(),
+                                        style: TextStyle(fontSize: 30),
+                                      )),
+                                onPressed: () {
+                                  setState(() {
+                                    if (i == Board.boardBase)
+                                      focussed.number = null;
+                                    else
+                                      focussed.number = i + 1;
+                                    focussed = null;
+                                    checkBoard();
+                                  });
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                      ))
+                    ])
+              : Container()),
+        ]),
+      ),
+      onWillPop: () async {
+        return true;
+      },
+    );
+  }
+}
